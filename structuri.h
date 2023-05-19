c34312b718cbc9603f07bfea25864a12c902f98e
gabriel16231 <128585958+gabriel16231@users.noreply.github.com>
2023-05-19 11:35:00 +0300

    task_2 function added

---
+++ structuri.h
@@ -0,0 +1,28 @@
+typedef struct p
+{
+    char *firstname,*secondname;
+   float points;
+   struct p *next;
+}Players;
+
+typedef struct t
+{
+    float team_points;
+    char *team_name;
+    int nr_players;
+    Players *player;
+    struct t *next;
+}Teams;
+
+typedef struct r
+{
+    Teams *team1,*team2;
+    struct r *next;
+}Rounds;
+
+typedef struct s
+{
+    Teams *team;
+    struct s *prev;
+}Stack;
+
