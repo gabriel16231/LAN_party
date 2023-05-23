35ce922372f11d28e0666906dd8dff7c8fdf1fa8
gabriel16231 <128585958+gabriel16231@users.noreply.github.com>
2023-05-23 22:50:15 +0300

    face AVL si afiseaza

--- cerinta_5.c
+++ cerinta_5.c
@@ -9,15 +9,28 @@ void sort_8(Tree *BST,Teams **node)
 
    }
 }
+void make_avl(Tree **AVL_root,Teams *team)
+{
+    *AVL_root=NULL;
+    add_in_tree(AVL_root,team->next->next->next->next);
+    add_in_tree(&(*AVL_root)->left,team->next->next);
+    add_in_tree(&(*AVL_root)->left->left,team->next);
+    add_in_tree(&(*AVL_root)->left->right,team->next->next->next);
+    add_in_tree(&(*AVL_root)->left->left->left,team);
+
+    add_in_tree(&(*AVL_root)->right,team->next->next->next->next->next->next);
+    add_in_tree(&(*AVL_root)->right->left,team->next->next->next->next->next);
+    add_in_tree(&(*AVL_root)->right->right,team->next->next->next->next->next->next->next);
+}
 void task_5(Tree *BST,FILE *out)
 {
     Tree *AVL;
     Teams *sorted_top8=NULL;
     sort_8(BST,&sorted_top8);
-    //display_teams(sorted_top8,out);
+    make_avl(&AVL,sorted_top8);
     fprintf(out,"\nTHE LEVEL 2 TEAMS ARE:\n");
-    fprintf(out,"%s\n",sorted_top8->next->next->next->next->next->next->next->team_name);
-    fprintf(out,"%s\n",sorted_top8->next->next->next->next->next->team_name);
-    fprintf(out,"%s\n",sorted_top8->next->next->next->team_name);
-    fprintf(out,"%s\n",sorted_top8->next->team_name);
+    fprintf(out,"%s\n",AVL->right->right->team->team_name);
+    fprintf(out,"%s\n",AVL->right->left->team->team_name);
+    fprintf(out,"%s\n",AVL->left->right->team->team_name);
+    fprintf(out,"%s\n",AVL->left->left->team->team_name);
 }   
